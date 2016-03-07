9ad9e9149a320318c3562a909199b72c54e71f2c
Verónica <verosgfc11@gmail.com>
2016-03-07 12:26:22 +0100

    Creado el constructor sin parámetros de la clase MiHashMap

---
+++ MiHashMap.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class MiHashMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiHashMap
+{
+    //Variable que representa la palabra clave.
+    private String clave;
+    //Variable que hace referencia al valor de la clave.
+    private int valor;
+    public MiHashMap(){
+        String clave = null;
+        int valor = 0;
+    }
+}
