739d073a0f27cd748dc7d223ce9a39288704430f
Verónica <verosgfc11@gmail.com>
2016-03-07 13:05:11 +0100

    Creado el metodo put

--- MiHashMap.java
+++ MiHashMap.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 /**
  * Write a description of class MiHashMap here.
  * 
@@ -8,11 +8,28 @@
 public class MiHashMap
 {
     //Variable que representa la palabra clave.
-    private String clave;
+    private String[] clave;
     //Variable que hace referencia al valor de la clave.
-    private int valor;
+    private int[] valor;
+    /**
+     * Constructor sin parámetros de la clase MiHashMap
+     */
     public MiHashMap(){
-        String clave = null;
-        int valor = 0;
+        clave = new String[0];
+        valor = new int[0];
+    }
+    
+    /**
+     * Método que asocia el valor especificado con la clave especificada. Si la clave existía, 
+     * entonces sobreescribe su valor y devuelve el valor antiguo. Si no existía devuelve -1. 
+     */
+    public int put(String claves, int valores){
+        int claveSi = -1;
+        for (int cont = 0; cont > clave.length; cont++) {
+          if (clave[cont] == claves) {
+                claveSi = cont;
+          }
+        }
+        return claveSi;
     }
 }
